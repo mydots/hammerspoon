0676fdc0c4577d05dd517e777d51282347488613
Franky W <frankywahl@users.noreply.github.com>
2020-09-11 12:18:30 -0700

    Use or statement

--- init.lua
+++ init.lua
@@ -227,11 +227,7 @@ hs.hotkey.bind({"alt"}, "m", function()
 		key = "Okta: Staging"
 	elseif string.match(text, "usertesting %- Extra Verification") then
 		key = "Okta: Production"
-	elseif string.match(text, "Zscaler Security") then
-		key = "Gmail"
-	elseif string.match(text, "Gmail") then
-		key = "Gmail"
-	elseif string.match(text, "Google") then
+	elseif string.match(text, "Gmail") or string.match(text, "Google") or string.match(text, "Zscaler Security") then
 		key = "Gmail"
 	elseif string.match(text, "LastPass") then
 		key = "LastPass"
